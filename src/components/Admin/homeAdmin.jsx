369d979351960adeb6da4dd8a14b57ca1d5e137e
HoangPham1208 <hoang.pham1208@hcmut.edu.vn>
2024-01-01 20:35:45 +0700

    admin branch homepage update

--- src/components/Admin/homeAdmin.jsx
+++ src/components/Admin/homeAdmin.jsx
@@ -1,19 +1,67 @@
 import React from "react";
 import { Navbar } from "../navbar";
-
+import { Button } from "flowbite-react";
+import { Card } from "flowbite-react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Cookies from "universal-cookie";
+import RefreshTokenAPI from "../Utils/token";
 export default function HomeAdmin() {
+  const cookie = new Cookies();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    RefreshTokenAPI();
+    axios
+      .get("http://localhost:4000/api/admin/showBranch", {
+        headers: {
+          Authorization: `Bearer ${cookie.get("accessToken")}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <>
       <Navbar />
-      <main className="mx-36 my-10">
-        <div className="my-5 font-semibold text-xl">
-          <button>Báo cáo</button>
+      <main className="mx-autoflex flex-col max-w-screen-xl pt-20 mx-36">
+        <div className="my-5 font-semibold text-3xl">
+          <button>Chi nhánh</button>
         </div>
-        <div className="my-5 font-semibold text-xl">
-          <button>Món hiện tại</button>
+        <div className="flex place-content-start gap-10 my-5">
+          <Button className="bg-[#6750A4] rounded-full border-[#6750A4] enabled:hover:bg-white enabled:hover:text-[#6750A4] ">
+            Thêm chi nhánh
+          </Button>
+          <Button className="text-[#6750A4] bg-white border-[#6750A4] rounded-full enabled:hover:bg-[#6750A4] enabled:hover:text-white">
+            Xóa chi nhánh
+          </Button>
         </div>
-        <div className="my-5 font-semibold text-xl">
-          <button>Sách hiện tại</button>
+        <div className="grid grid-cols-3 gap-10">
+          {data.map((item) => (
+            <div>
+              <Card
+                className="max-w-sm"
+                imgAlt="Meaningful alt text for an image that is not purely decorative"
+                imgSrc="/image1.png"
+              >
+                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {item.address}
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                  <div>
+                    ID: {item.branchId} <br/>
+                    Manager id: {item.managerId} <br/>
+                    working time: {item.workingTime} <br/>
+                    createDate: {item.createDate}
+                  </div>
+                </p>
+              </Card>
+            </div>
+          ))}
         </div>
       </main>
     </>
--- src/components/Manager/staff.jsx
+++ src/components/Manager/staff.jsx
@@ -5,44 +5,40 @@ import { Checkbox, Table } from "flowbite-react";
 import { useNavigate } from "react-router-dom";
 import ListFunc from "../Utils/listFunc";
 import RefreshTokenAPI from "../Utils/token";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import Cookies from "universal-cookie";
-
 
 export default function Staff() {
   const navigate = useNavigate();
   const [items2, setItems2] = useState([]);
   const cookie = new Cookies();
   useEffect(() => {
-    axios.get("http://localhost:4000/api/manager/showStaff", {
-      headers: {
-        Authorization: `Bearer ${cookie.get("accessToken")}`,
-      },
-    })
-    .then((res) => {
-      console.log(res.data);
-      setItems2(res.data);
-    }
-    )
-    .catch((err) => {
-      console.log(err);
-    });
+    axios
+      .get("http://localhost:4000/api/manager/showStaff", {
+        headers: {
+          Authorization: `Bearer ${cookie.get("accessToken")}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setItems2(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
   return (
     <div>
       <Navbar />
       <main className="mx-auto flex flex-col max-w-screen-xl pt-20">
-       <ListFunc />
+        <ListFunc />
         <div className="flex ml-36 gap-4">
           <Button className="bg-[#6750A4] rounded-full border-[#6750A4] enabled:hover:bg-white enabled:hover:text-[#6750A4] ">
             Thêm nhân viên
           </Button>
           <Button className="text-[#6750A4] bg-white border-[#6750A4] rounded-full enabled:hover:bg-[#6750A4] enabled:hover:text-white">
             Xóa nhân viên
-          </Button>
-          <Button className="text-[#6750A4] bg-white border-[#6750A4] rounded-full enabled:hover:bg-[#6750A4] enabled:hover:text-white">
-            Chi tiết
           </Button>
         </div>
         <div className="relative text-gray-600 mx-36 my-7">
@@ -73,13 +69,13 @@ export default function Staff() {
                     <Table.Cell>{item.staffName}</Table.Cell>
                     <Table.Cell>{item.email}</Table.Cell>
                     <Table.Cell>{item.address}</Table.Cell>
-                    <Table.Cell>{
-                      new Date(item.workingDate).toLocaleDateString("en-GB", {
+                    <Table.Cell>
+                      {new Date(item.workingDate).toLocaleDateString("en-GB", {
                         year: "numeric",
                         month: "long",
                         day: "numeric",
-                      })
-                    }</Table.Cell>
+                      })}
+                    </Table.Cell>
                   </Table.Row>
                 </>
               ))}
