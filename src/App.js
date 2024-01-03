8d6e269f7a9d02e25a2623c4a94c0a2015a232d7
Daniel Roussielle <9989840+droussielle@users.noreply.github.com>
2024-01-03 23:35:57 +0700

    flex-wrap on categories

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ import OrderLocationHistoryManager from "./components/Manager/historyLocation";
 import HomeAdmin from "./components/Admin/homeAdmin";
 import AddBranch from "./components/Admin/AddBranch";
 import BookList from "./components/Admin/BookList";
-import AddBook from "./components/Admin/addBook";
+import AddBook from "./components/Admin/addbook";
 import BookDetailsModify from "./components/Admin/bookDetailModify";
 import MenuDrink from "./components/Admin/menuDrink";
 import AddBookOrder from "./components/Staff/addBookOrder";
--- src/components/Guest/category.jsx
+++ src/components/Guest/category.jsx
@@ -1,31 +1,33 @@
 export default function Category() {
   return (
     <>
-      <h2 className="text-black text-2xl not-italic font-bold mb-5">
-        Đọc theo thể loại
-      </h2>
-      <div
-        className="flex flex-wrap gap-x-5 gap-y-4 grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 w-full text-xl items-center align-middle font-bookTitle pb-2"
-        id="book-type-content-grid"
-      >
-        <button className="shadow-2 hover:shadow-4 h-16 w-44 rounded-xl transition-all ease-m3-standard-accelerate">
-          Tiểu sử
-        </button>
-        <button className="shadow-2 hover:shadow-4 h-16 w-44 rounded-xl transition-all ease-m3-standard-accelerate">
-          Khoa học
-        </button>
-        <button className="shadow-2 hover:shadow-4 h-16 w-44 rounded-xl transition-all ease-m3-standard-accelerate">
-          Văn học
-        </button>
-        <button className="shadow-2 hover:shadow-4 h-16 w-44 rounded-xl transition-all ease-m3-standard-accelerate">
-          Lịch sử
-        </button>
-        <button className="shadow-2 hover:shadow-4 h-16 w-44 rounded-xl transition-all ease-m3-standard-accelerate">
-          Lãng mạn
-        </button>
-        <button className="shadow-2 hover:shadow-4 h-16 w-44 rounded-xl transition-all ease-m3-standard-accelerate">
-          Viễn tưởng
-        </button>
+      <div className="space-y-2">
+        <h2 className="text-black text-2xl not-italic font-bold">
+          Đọc theo thể loại
+        </h2>
+        <div
+          className="flex flex-row overflow-auto p-2 gap-x-5 w-full text-xl items-center align-middle font-bookTitle"
+          id="book-type-content-grid"
+        >
+          <button className="shadow-2 hover:shadow-4 h-14 w-40 md:h-16 md:w-44 rounded-xl transition-all ease-m3-standard-accelerate shrink-0">
+            Tiểu sử
+          </button>
+          <button className="shadow-2 hover:shadow-4 h-14 w-40 md:h-16 md:w-44 rounded-xl transition-all ease-m3-standard-accelerate shrink-0">
+            Khoa học
+          </button>
+          <button className="shadow-2 hover:shadow-4 h-14 w-40 md:h-16 md:w-44 rounded-xl transition-all ease-m3-standard-accelerate shrink-0">
+            Văn học
+          </button>
+          <button className="shadow-2 hover:shadow-4 h-14 w-40 md:h-16 md:w-44 rounded-xl transition-all ease-m3-standard-accelerate shrink-0">
+            Lịch sử
+          </button>
+          <button className="shadow-2 hover:shadow-4 h-14 w-40 md:h-16 md:w-44 rounded-xl transition-all ease-m3-standard-accelerate shrink-0">
+            Lãng mạn
+          </button>
+          <button className="shadow-2 hover:shadow-4 h-14 w-40 md:h-16 md:w-44 rounded-xl transition-all ease-m3-standard-accelerate shrink-0">
+            Viễn tưởng
+          </button>
+        </div>
       </div>
     </>
   );
