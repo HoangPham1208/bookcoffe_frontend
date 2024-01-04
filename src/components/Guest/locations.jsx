815cd527aaf66afc49811517ff433f1376ab451b
Daniel Roussielle <9989840+droussielle@users.noreply.github.com>
2024-01-04 14:42:22 +0700

    locations

--- src/components/Guest/locations.jsx
+++ src/components/Guest/locations.jsx
@@ -32,55 +32,57 @@ export default function Locations() {
     <>
       <Navbar mode="login" />
       <section className="mx-auto px-6 md:px-10 py-10 space-y-6 flex flex-col max-w-screen-xl pt-20">
-        <main className="my-5">
+        <main className="my-5 space-y-5">
           <div className="w-full font-bold text-3xl">Chi nhánh</div>
-          <div className="grid grid-cols-3 gap-10">
-          {data.map((item) => (
-            <div>
-              <Card
-                className="max-w-sm"
-                imgAlt={
-                  "This is a photo of a " +
-                  item.address +
-                  " branch"
-                }
-                imgSrc={
-                  "http://localhost:4000/api/customer/getBranchImage/" + item.branchId
-                }
-              >
-                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {item.address}
-                </h5>
-                <div className="font-normal text-gray-700 dark:text-gray-400">
-                  <div className="flex justify-between">
+          <div className="flex max-sm:flex-col flex-wrap max-sm:gap-y-5 sm:gap-5">
+            {data.map((item) => (
+              <div>
+                <Card
+                  className="rounded-xl w-full sm:w-80 "
+                  renderImage={() => (
+                    <img
+                      className="object-cover h-64 shrink-0 border-b overflow-hidden rounded-t-xl"
+                      src={
+                        "http://localhost:4000/api/customer/getBranchImage/" +
+                        item.branchId
+                      }
+                      alt="branch number 1"
+                    />
+                  )}
+                >
+                  <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {item.address}
+                  </h5>
+                  <div className="font-normal text-gray-700 dark:text-gray-400">
+                    <div className="flex justify-between">
+                      <div>
+                        <span className="font-semibold">Quản lý: </span>
+                        {item.managerName}
+                      </div>
+                    </div>
                     <div>
-                      <span className="font-semibold">Quản lý: </span>
-                      {item.managerName}
+                      <span className="font-semibold">Giờ hoạt động: </span>{" "}
+                      {item.workingTime} <br />
+                    </div>
+                    <div>
+                      <span className="font-semibold">Số điện thoại: </span>{" "}
+                      {item.phoneNumber} <br />
+                    </div>
+                    <div className="flex place-content-end pt-2">
+                      <Button
+                        onClick={() => Navigate("/")}
+                        theme={customTheme}
+                        color="primary"
+                        pill
+                      >
+                        Đặt chỗ
+                      </Button>
                     </div>
                   </div>
-                  <div>
-                    <span className="font-semibold">Giờ hoạt động: </span>{" "}
-                    {item.workingTime} <br />
-                  </div>
-                  <div>
-                    <span className="font-semibold">Số điện thoại: </span>{" "}
-                    {item.phoneNumber} <br />
-                  </div>
-                  <div className="flex place-content-end gap-10 my-5">
-                    <Button
-                      onClick={()=>Navigate("/")}
-                      theme={customTheme}
-                      color="primary"
-                      pill
-                    >
-                      Đặt chỗ
-                    </Button>
-                  </div>
-                </div>
-              </Card>
-            </div>
-          ))}
-        </div>
+                </Card>
+              </div>
+            ))}
+          </div>
         </main>
       </section>
     </>
