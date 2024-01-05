6dd10bb6a1758584d445ec048e640b8670a24648
Daniel Roussielle <9989840+droussielle@users.noreply.github.com>
2024-01-05 12:31:08 +0700

    layout change for admin

--- src/components/Admin/menuDrink.jsx
+++ src/components/Admin/menuDrink.jsx
@@ -47,79 +47,84 @@ export default function MenuDrink() {
   return (
     <>
       <Navbar />
-      <main className="mx-autoflex flex-col max-w-screen-xl py-20 mx-36">
-        <div className="my-5 font-semibold text-3xl mx-36">
-          <button>Menu</button>
-        </div>
-        <div className="flex place-content-start gap-10 my-5 mx-36">
-          <Button theme={customTheme} color="primary" pill
-          onClick={() => {
-            navigate("/admin/menuDrink/addDrink");
-          }}
-          >
-            Thêm nước
-          </Button>
-        </div>
-        <div className="relative text-gray-600 my-7 mx-36">
-          <input
-            type="search"
-            name="serch"
-            placeholder="Tìm kiếm với tên nước"
-            className="bg-gray-100 rounded-full text-sm focus:outline-none w-full px-5 h-12"
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                handleSearch(e.target.value);
-              }
-            }}
-            onChange={(e) => {
-              if (e.target.value === "") setResult(items);
-            }}
-          />
-        </div>
-        <div className="overflow-x-auto mx-36">
-          <Table hoverable>
-            <Table.Head className="text-center">
-              <Table.HeadCell></Table.HeadCell>
-              {/* <Table.HeadCell>ID</Table.HeadCell> */}
+      <section className="mx-auto px-6 md:px-10 py-10 space-y-6 flex flex-col max-w-screen-xl pt-20">
+        <main className="my-5 space-y-5">
+          <div className="font-semibold text-3xl">
+            <button>Menu</button>
+          </div>
+          <div className="flex place-content-start gap-10 my-5">
+            <Button
+              theme={customTheme}
+              color="primary"
+              pill
+              onClick={() => {
+                navigate("/admin/menuDrink/addDrink");
+              }}
+            >
+              Thêm nước
+            </Button>
+          </div>
+          <div className="relative text-gray-600 my-7">
+            <input
+              type="search"
+              name="serch"
+              placeholder="Tìm kiếm với tên nước"
+              className="bg-gray-100 rounded-full text-sm focus:outline-none w-full px-5 h-12"
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleSearch(e.target.value);
+                }
+              }}
+              onChange={(e) => {
+                if (e.target.value === "") setResult(items);
+              }}
+            />
+          </div>
+          <div className="overflow-x-auto">
+            <Table hoverable>
+              <Table.Head className="text-center">
+                <Table.HeadCell></Table.HeadCell>
+                {/* <Table.HeadCell>ID</Table.HeadCell> */}
 
-              <Table.HeadCell>Tên</Table.HeadCell>
-              <Table.HeadCell>Size</Table.HeadCell>
-              <Table.HeadCell>Giá</Table.HeadCell>
-              <Table.HeadCell className="p-4"></Table.HeadCell>
-            </Table.Head>
-            <Table.Body className="divide-y text-center">
-              {result &&
-                result.map((item1, index1) =>
-                  items[index1].price.map((item2, index2) => (
-                    <>
-                      <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                        <Table.Cell className="grid justify-center">
-                          {/* search by id like this :
+                <Table.HeadCell>Tên</Table.HeadCell>
+                <Table.HeadCell>Size</Table.HeadCell>
+                <Table.HeadCell>Giá</Table.HeadCell>
+                <Table.HeadCell className="p-4"></Table.HeadCell>
+              </Table.Head>
+              <Table.Body className="divide-y text-center">
+                {result &&
+                  result.map((item1, index1) =>
+                    items[index1].price.map((item2, index2) => (
+                      <>
+                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                          <Table.Cell className="grid justify-center">
+                            {/* search by id like this :
                           http://localhost:4000/api/staff/getDrinksImage/item.drinksId
                            */}
-                          <img
-                            src={`http://localhost:4000/api/staff/getDrinksImage/${item1.drinksId}`}
-                            alt={item1.image}
-                            className="w-16 h-16 rounded-full"
-                          />
-                        </Table.Cell>
-                        {/* <Table.Cell>{item1.drinksId}</Table.Cell> */}
-                        <Table.Cell>{item1.drinksName}</Table.Cell>
-                        <Table.Cell>{item1.size[index2]}</Table.Cell>
-                        <Table.Cell>
-                          {new Intl.NumberFormat("vi-VN", {
-                            style: "currency",
-                            currency: "VND",
-                          }).format(item1.price[index2])}
-                        </Table.Cell>
-                      </Table.Row>
-                    </>
-                  ))
-                )}
-            </Table.Body>
-          </Table>
-        </div>
-      </main>
+                            <img
+                              src={`http://localhost:4000/api/staff/getDrinksImage/${item1.drinksId}`}
+                              alt={item1.image}
+                              className="w-16 h-16 rounded-full"
+                            />
+                          </Table.Cell>
+                          {/* <Table.Cell>{item1.drinksId}</Table.Cell> */}
+                          <Table.Cell>{item1.drinksName}</Table.Cell>
+                          <Table.Cell>{item1.size[index2]}</Table.Cell>
+                          <Table.Cell>
+                            {new Intl.NumberFormat("vi-VN", {
+                              style: "currency",
+                              currency: "VND",
+                            }).format(item1.price[index2])}
+                          </Table.Cell>
+                        </Table.Row>
+                      </>
+                    ))
+                  )}
+              </Table.Body>
+            </Table>
+          </div>
+        </main>
+      </section>
     </>
   );
 }
